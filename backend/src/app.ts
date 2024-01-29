fb5af5122630cdd6d2cae82962e7442887453f0a
Raymie Segars <raymievsegarsiv@gmail.com>
2024-01-29 06:08:11 -0500

    Backend set up with database and error handling
    First backend commit for the notes web app

---
+++ backend/src/app.ts
@@ -0,0 +1,30 @@
+import 'dotenv/config';
+import express, { Request, Response, NextFunction } from 'express';
+import notesRoutes from "./routes/notes";
+import morgan from "morgan";
+import createHttpError, { isHttpError } from 'http-errors';
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use("/api/notes", notesRoutes);
+
+app.use((req, res, next) => {
+  next(createHttpError(404, "Endpoint not found"));
+});
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+    let errorMessage = "An unknown error occurred";
+    let statusCode = 500;
+    if (isHttpError(error)) {
+      statusCode = error.status;
+      errorMessage = error.message;
+    }
+    res.status(statusCode).json({ error: errorMessage });
+});
+
+export default app;
