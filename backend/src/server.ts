b0411065f567e53f8c05973c5ebb932b670c9a23
Raymie Segars <raymievsegarsiv@gmail.com>
2024-01-29 02:50:59 -0500

    Initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("");
+});
+
+app.listen(port, () => {
+  console.log("Server running on port: " + port);
+});
