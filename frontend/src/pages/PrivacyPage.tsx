41a6096115afd11ff68e342ae10ab8b606c61b32
Raymie Segars <raymievsegarsiv@gmail.com>
2024-02-05 15:27:19 -0500

    Finished design for Note-It application
    More upgrades can be made but this is the functional app finished.

---
+++ frontend/src/pages/PrivacyPage.tsx
@@ -0,0 +1,9 @@
+const PrivacyPage = () => {
+  return ( 
+    <div>
+      <p>We care about your privacy, however we take no liability for your notes or data.</p>
+    </div>
+   );
+}
+ 
+export default PrivacyPage;
