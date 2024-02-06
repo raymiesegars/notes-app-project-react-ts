2a36d631ba13b19858c197cc80ce9ff0e2c15567
Raymie Segars <raymievsegarsiv@gmail.com>
2024-02-05 20:47:03 -0500

    Note-It 1.1
    Added a modal confirmation window when deleting notes.

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -4,15 +4,28 @@ import { Card } from "react-bootstrap";
 import { Note as NoteModel } from "../models/note";
 import { formatDate } from "../utils/formatDate";
 import { MdDelete } from "react-icons/md";
+import { useState } from "react";
+import { Modal, Button } from 'react-bootstrap';
 
-interface NoteProps { 
+interface NoteProps {
   note: NoteModel,
   onNoteClicked: (note: NoteModel) => void,
   onDeleteNoteClicked: (note: NoteModel) => void,
-  className?:string,
+  className?: string,
 }
 
 const Note = ({ note, onNoteClicked, onDeleteNoteClicked, className }: NoteProps) => {
+
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+
+  const handleShowConfirmModal = () => setShowConfirmModal(true);
+  const handleCloseConfirmModal = () => setShowConfirmModal(false);
+
+  const handleConfirmDelete = () => {
+    onDeleteNoteClicked(note);
+    handleCloseConfirmModal();
+  }
+
   const {
     title,
     text,
@@ -28,28 +41,46 @@ const Note = ({ note, onNoteClicked, onDeleteNoteClicked, className }: NoteProps
   }
 
   return (
-    <Card 
-    className={`${styles.noteCard} ${className}`}
-    onClick={() => onNoteClicked(note)}>
-      <Card.Body className={styles.cardBody}>
-        <Card.Title className={styleUtils.flexCenter}>
-          {title}
-          <MdDelete 
-          className="text-muted ms-auto"
-          onClick={(e) => {
-            onDeleteNoteClicked(note);
-            e.stopPropagation();
-          }}
-          />
-        </Card.Title>
-        <Card.Text className={styles.cardText}>
-          {text}
-        </Card.Text>
-      </Card.Body>
-      <Card.Footer className="text-muted">
-        {createdUpdatedText}
-      </Card.Footer>
-    </Card>
+    <>
+      <Card
+        className={`${styles.noteCard} ${className}`}
+        onClick={() => onNoteClicked(note)}>
+        <Card.Body className={styles.cardBody}>
+          <Card.Title className={styleUtils.flexCenter}>
+            {title}
+            <MdDelete
+              className="text-muted ms-auto"
+              onClick={(e) => {
+                e.stopPropagation();
+                handleShowConfirmModal();
+              }}
+            />
+          </Card.Title>
+          <Card.Text className={styles.cardText}>
+            {text}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer className="text-muted">
+          {createdUpdatedText}
+        </Card.Footer>
+      </Card>
+
+      <Modal show={showConfirmModal} onHide={handleCloseConfirmModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Deletion</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete this note?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseConfirmModal}>
+            No
+          </Button>
+          <Button variant="primary" onClick={handleConfirmDelete}>
+            Yes
+          </Button>
+        </Modal.Footer>
+
+      </Modal>
+    </>
   )
 }
 
