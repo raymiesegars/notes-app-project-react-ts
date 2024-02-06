6fec1894aba450bdfd278f9a8c344dc9c2d66e86
Raymie Segars <raymievsegarsiv@gmail.com>
2024-02-06 09:05:33 -0500

    privacy text fix for dark mode

--- frontend/src/pages/PrivacyPage.tsx
+++ frontend/src/pages/PrivacyPage.tsx
@@ -1,6 +1,12 @@
+import React from "react";
+import '../styles/Dark.mode.css';
+
 const PrivacyPage = () => {
+
+  const isDarkMode = document.body.classList.contains('body-dark-mode');
+
   return ( 
-    <div>
+    <div className={isDarkMode ? 'privacy-page-dark' : ''}>
       <p>We care about your privacy, however we take no liability for your notes or data.</p>
     </div>
    );
