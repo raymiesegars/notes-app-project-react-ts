3ee0ceb189664543ab72216772dbd8eefa1f05e1
Raymie Segars <raymievsegarsiv@gmail.com>
2024-02-06 09:13:52 -0500

    fixed dark mode login text

--- frontend/src/components/NotesPageLoggedOutView.tsx
+++ frontend/src/components/NotesPageLoggedOutView.tsx
@@ -1,7 +1,15 @@
+import React from 'react';
+import '../styles/Dark.mode.css';
+
 const NotesPageLoggedOutView = () => {
-  return ( 
-    <p>Please login to see your notes</p>
+
+  const isDarkMode = document.body.classList.contains('body-dark-mode');
+
+  return (
+    <div className={isDarkMode ? 'notes-page-logged-out-view' : ''}>
+      <p>Please login to see your notes</p>
+    </div>
    );
 }
- 
+
 export default NotesPageLoggedOutView;
